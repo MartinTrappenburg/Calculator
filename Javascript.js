a4c9bd0e908490dfdb20067bdd6304c3f2551d24
ASSASSINS-CREED\Martin <martintrap1@gmail.com>
2017-03-16 11:48:52 +0100

    TEST

---
+++ Javascript.js
@@ -0,0 +1,30 @@
+var display = document.getElementById("Display");
+function Buttons (F) {
+	display.innerHTML = display.innerHTML + F;
+}
+console.log("testing")
+function Operator1 (F) {
+	display.innerHTML = display.innerHTML + F;
+	Getal1 = display.innerHTML;
+}
+function Operator2 (F) {
+	display.innerHTML = display.innerHTML + F;
+	Getal1 = display.innerHTML;
+}
+function Operator3 (F) {
+	display.innerHTML = display.innerHTML + F;
+	Getal2 = display.innerHTML;
+}
+function Operator4 (F) {
+	display.innerHTML = display.innerHTML + F;
+	Getal1 = display.innerHTML;
+}
+function Operator5 (F) {
+	display.innerHTML = display.innerHTML + F;
+	Getal1 = display.innerHTML;
+}
+function Operator6 (F) {
+	display.innerHTML = '';
+}
+var Getal1 = '';
+var Getal2 = '';
