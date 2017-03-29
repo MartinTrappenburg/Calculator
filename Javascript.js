c7a81c7907cf9fbbae0999b7b037544a69fab2c1
ASSASSINS-CREED\Martin <martintrap1@gmail.com>
2017-03-29 08:56:08 +0200

    JavaScript Calculator finished

--- Javascript.js
+++ Javascript.js
@@ -1,29 +1,56 @@
 var display = document.getElementById("Display");
+var Ans = 0;
+var Operator = '';
 function Buttons (F) {
 	display.innerHTML = display.innerHTML + F;
 }
 console.log("testing")
 function Operator1 (F) {
-	display.innerHTML = display.innerHTML + F;
 	Getal1 = display.innerHTML;
+	Operator = '+';
+	console.log('Getal1='+Getal1);
+	display.innerHTML='';
+
 }
 function Operator2 (F) {
-	display.innerHTML = display.innerHTML + F;
 	Getal1 = display.innerHTML;
+	Operator = '-';
+	display.innerHTML='';
 }
 function Operator3 (F) {
-	display.innerHTML = display.innerHTML + F;
+	
 	Getal2 = display.innerHTML;
+	console.log('Getal2='+Getal2);
+	if (Operator == '+') {
+		Ans = +Getal1 + +Getal2;
+		display.innerHTML = Ans;
+	}
+		if (Operator == '-') {
+		Ans = +Getal1 - +Getal2;
+		display.innerHTML = Ans;
+	}
+		if (Operator == '*') {
+		Ans = Math.round(+Getal1) * +Getal2;
+		display.innerHTML = Ans;
+	}
+		if (Operator == '/') {
+		Ans = +Getal1 / +Getal2;
+		display.innerHTML = Ans;
+	}
 }
+
 function Operator4 (F) {
-	display.innerHTML = display.innerHTML + F;
 	Getal1 = display.innerHTML;
+	Operator = '*';
+	display.innerHTML='';
 }
 function Operator5 (F) {
-	display.innerHTML = display.innerHTML + F;
 	Getal1 = display.innerHTML;
+	Operator = '/';
+	display.innerHTML='';
 }
-function Operator6 (F) {
+
+function Operator7 (F) {
 	display.innerHTML = '';
 }
 var Getal1 = '';
